766ebb38e34c88ef5ec2746b93ae2d031bf08c08
Justin Murdock <jmurdock@lyris.com>
2015-07-28 23:53:25 -0700

    challenge 2: baby steps

--- node1.js
+++ node1.js
@@ -1,1 +1,8 @@
-console.log("");
+var total = 0;
+for(var i = 0, len = process.argv.length; i < len; i++){
+    if(i >= 2){
+        total += Number(process.argv[i]);
+    }
+}
+
+console.log(total);
