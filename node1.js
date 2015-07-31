932a3813d5d818680a0636d8e2af9929f4430031
Justin Murdock <jmurdock@lyris.com>
2015-07-31 11:50:35 -0700

    challenge 11: http uppercaser-er

--- node1.js
+++ node1.js
@@ -1,9 +1,10 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map')
 
 var server = http.createServer(function(req, res){
     //socket logic
-    var src = fs.createReadStream(process.argv[3]);
-    src.pipe(res);
+    req.pipe(map(function(chunk){
+        return chunk.toString().toUpperCase()
+    })).pipe(res)
 });
 server.listen(Number(process.argv[2]));
