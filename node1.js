7b8f332c0932cca1faae5df9c9c4a554824a293f
Justin Murdock <jmurdock@lyris.com>
2015-07-31 11:15:45 -0700

    challenge 9: time server

--- node1.js
+++ node1.js
@@ -1,28 +1,19 @@
-var http = require('http'),
-    request = [],
-    dataStrings = {},
-    orderOfServerInputs = [];
-var completeResponses = [];
-for(server in process.argv){
-    if(server >= 2){
-        
-        orderOfServerInputs[orderOfServerInputs.length] = process.argv[server].substr(-4);
-        request[request.length] = function(thisServer){
-            http.get(process.argv[server], function(response){
-                response.on("data", function(data){
-                    dataStrings[thisServer.substr(-4)] = dataStrings[thisServer.substr(-4)] ? dataStrings[thisServer.substr(-4)] : ''; //if not set set to ''
-                    dataStrings[thisServer.substr(-4)] += data.toString();
-                });
-                response.on("end", function(data){
-                    completeResponses[completeResponses.length] = true;
-                    if(completeResponses.length > 2){
-                        for(item in orderOfServerInputs){
-                            serverNo = orderOfServerInputs[item].substr(-4)
-                            console.log(dataStrings[serverNo]);
-                        }
-                    }
-                });
-            }); 
-        }(process.argv[server]);
-    }
-}
+var net = require('net')
+var twoDigit = function(input){
+    return (input < 10 ? '0' : '') + input;
+};
+
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+var hour = date.getHours();
+var minutes = date.getMinutes();
+
+var data = [year, twoDigit(month), twoDigit(day)].join('-') + " " + [twoDigit(hour), twoDigit(minutes)].join(":");
+
+var server = net.createServer(function(socket){
+    //socket logic
+    socket.end(data.toString());
+});
+server.listen(Number(process.argv[2]));
