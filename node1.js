e20de703ba1a0c506d6c7e4aba88256fde9bab1f
Justin Murdock <jmurdock@lyris.com>
2015-07-30 21:16:33 -0700

    challenge 7: http get w node stream

--- node1.js
+++ node1.js
@@ -1,1 +1,6 @@
-var mymodule = require('./mymodule');
+var http = require('http'),
+    request = http.get(process.argv[2], function(response){
+        response.on("data", function(data){
+            console.log(data.toString());
+        })
+    });
